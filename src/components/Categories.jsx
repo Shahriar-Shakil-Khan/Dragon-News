dc6958ce83e27d6be7f2f60ad586dc6c46a650fd
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-08-15 08:58:41 +0600

    shakil

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { use } from 'react';
+
+const categoryPromise = fetch('/categories.json')
+    .then(res => res.json())
+    // .then(data => data.data);
 
 const Categories = () => {
+    console.log(categoryPromise)
+    const categories = use(categoryPromise);
     return (
         <div>
-            <h2 className='font-bold'>All Categories</h2>
+            <h2 className='font-bold'>All Categories ({categories.length})</h2>
         </div>
     );
 };
